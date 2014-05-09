eecc58fe5fb516e6220860644a95a84a5c661811
kkkon <kiyofumi.kondoh+github@gmail.com>
2014-05-10 04:55:23 +0900

    add break-inheritance

---
+++ break-inheritance/sub-a/src/main/java/jp/ne/sakura/kkkon/test/GitHubMavenPluginMultiModule/App.java
@@ -0,0 +1,73 @@
+/*
+ * The MIT License
+ * 
+ * Copyright (C) 2014 Kiyofumi Kondoh
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package jp.ne.sakura.kkkon.test.GitHubMavenPluginMultiModule;
+
+import java.io.File;
+
+/**
+ *
+ * @author Kiyofumi Kondoh
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        AppOption appOpt = new AppOption();
+        appOpt.createOptions();
+        {
+            final boolean result = appOpt.parseOption( args );
+            if ( false == result )
+            {
+                appOpt.showUsage();
+                return;
+            }
+        }
+        appOpt.applyOption();
+
+        if ( null == args )
+        {
+            appOpt.showUsage();
+            return;
+        }
+        if ( args.length < 1 )
+        {
+            appOpt.showUsage();
+            return;
+        }
+
+        {
+            final String[] remain_args = appOpt.getArgs();
+            if ( null != remain_args )
+            {
+                final int count = remain_args.length;
+                for ( int index = 0; index < count; ++index )
+                {
+                    final String arg = remain_args[index];
+                    //stripRecursive( appOpt, null, arg );
+                }
+            }
+        }
+    }
+}
